4b0dea6dfed03561beb55e861407281e0a88be00
BryanAsf <80415153+bryanasfuk@users.noreply.github.com>
2021-03-26 06:54:39 +0700

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -49,7 +49,7 @@ Hello! my name *{}*.
 And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
 
-TECHNO_IMG = "https://telegra.ph/file/33d17d96b2e69dd17b033.jpg"
+TECHNO_IMG = "https://telegra.ph/file/9745bbbf6f6d3393358d1.jpg"
 IMPORTED = {}
 MIGRATEABLE = []
 HELPABLE = {}
